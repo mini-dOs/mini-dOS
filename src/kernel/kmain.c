e6733422bca94436c2bb9025b503f91ed8f15b57
bogamie <bogamie02@gmail.com>
2026-03-20 16:51:46 +0900

    Revert "feat(memory): implement paging system and memory management functions"
    This reverts commit 8e390b16312c1ecdf95742b0802927eccfef95c7.

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -6,7 +6,6 @@
 #include <interrupt_init.h>
 #include <serial.h>
 #include <multiboot.h>
-#include <mm/paging.h>
 
 /* Called from boot.s with Multiboot2 magic in RDI, info physical addr in RSI */
 void kmain(uint32_t multiboot_magic, uint32_t multiboot_info) {
@@ -25,10 +24,6 @@ void kmain(uint32_t multiboot_magic, uint32_t multiboot_info) {
     multiboot_parse((void *)(uint64_t)multiboot_info);
 
     serial_write("Memory parsing done\r\n");
-
-    paging_init();
-
-    serial_write("Paging initialized\r\n");
 
     init_interrupt_subsystem();
 
