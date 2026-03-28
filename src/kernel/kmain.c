7007175d76c60de9bfa27a1446f302e603fec8a7
bogamie <bogamie02@gmail.com>
2026-03-28 16:31:06 +0900

    feat(memory): enhance kmain with test for mapped access and page fault

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -35,7 +35,19 @@ void kmain(uint32_t multiboot_magic, uint32_t multiboot_info) {
     // Turn on the Interrupt Switch of CPU
     sti();
 
-    serial_write("Interrupts enabled\r\n");
+    serial_write("Interrupts enabled\r\n\n");
+
+    static volatile uint64_t mapped_probe = 0x1122334455667788ULL;
+    serial_write("[Test] mapped access start\r\n");
+    mapped_probe ^= 0x55AA55AA55AA55AAULL;
+    serial_write("[Test] mapped value = ");
+    serial_write_hex64(mapped_probe);
+    serial_write("\r\n\n");
+
+    serial_write("[Test] trigger page fault\r\n");
+    volatile uint64_t *bad = (volatile uint64_t *)0x40000000ULL; // 1GiB
+    *bad = 0xDEADBEEFCAFEBABEULL; // 의도적 #PF (err_code=0x2)
+    serial_write("[TEST] unreachable\r\n");
 
     while (1)
         hlt();
